2cb6e67c51e90f9d36037e25257456082d10d1b5
HanningtoneM <hanningtonepompyy@gmail.com>
2023-06-23 15:12:27 +0300

    Add card arrays

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 let firstCard = 9
 let secondCard = 12
 let sum = firstCard + secondCard
+let cards = [firstCard, secondCard] // array an ordered list of items
 let hasBlackJack = false
 let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
+let sumEl = document.querySelector("#sum-el")
+let cardsEl = document.getElementById("cards-el")
 
 function startGame() {
+    renderGame()
+}
+
+function renderGame() {
+    // render out first and second card
+    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+    // render out all the cards
+    sumEl.textContent = "Sum: " + sum
     if(sum <= 20) {
         message = "draw new card?"
     }else if(sum === 21) {
@@ -18,6 +29,13 @@ function startGame() {
     }
 
     messageEl.textContent = message
+}
+
+function newCard() {
+    console.log("Drawing new card")
+    let card = 5
+    sum += card
+    renderGame()
 }
 
 
